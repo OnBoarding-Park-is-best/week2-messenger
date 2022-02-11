8311e6b8037046c6fa956e39cd67afe14de55828
고동현 <dhjk35@naver.com>
2022-02-12 00:27:20 +0900

    :recycle: modal reducer 변경으로 인한 message 컴포넌트 수정

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -8,46 +8,41 @@ import { ellipsisString } from '~utils/message';
 const useMessage = () => {
   const dispatch = useDispatch();
   const state = useSelector((state: RootStateType) => state.modal);
-  const messageId = useRef('');
-  const [content, setContent] = useState<string>('');
 
-  const handleClickDelete = useCallback(
+  const handleDelete = useCallback(
     (e: React.MouseEvent) => {
       const container: HTMLDivElement = (e.target as HTMLElement).closest(
         '[data-message-id]',
       )!;
       const messageContainer: HTMLDivElement =
         container.querySelector('[data-is-message]')!;
-      messageId.current = container.dataset.messageId!;
+      const messageId = container.dataset.messageId!;
       const message = messageContainer.innerText;
-      setContent(`${ellipsisString(message)} 메시지를 삭제하시겠습니까?`);
-      dispatch(showModal());
+
+      const handleDelete = () => {
+        dispatch(deleteMessage({ messageId }));
+        dispatch(closeModal());
+      };
+
+      dispatch(
+        showModal({
+          isModalOpen: true,
+          content: `${ellipsisString(message)} 메시지를 삭제하시겠습니까?`,
+          onSubmit: handleDelete,
+        }),
+      );
     },
     [dispatch],
   );
-
-  const handleDelete = useCallback(() => {
-    dispatch(deleteMessage({ messageId: messageId.current }));
-    dispatch(closeModal());
-    messageId.current = '';
-  }, [dispatch]);
 
   const handleReply = useCallback(() => {
     // TODO: Message 상위 컴포넌트에서 구현 필요
     console.log('Reply clicked');
   }, []);
 
-  const handleClose = useCallback(() => {
-    dispatch(closeModal());
-  }, [dispatch]);
-
   return {
-    handleClickDelete,
     handleDelete,
     handleReply,
-    handleClose,
-    isModalOpen: state.showModal,
-    modalContent: content,
   };
 };
 
